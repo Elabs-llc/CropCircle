f92048bd8aeaab0dfec16b21cb3fc3d740103f65
Arhin David Kwabena <davidarhin2005@gmail.com>
2024-11-09 12:54:38 +0000

    routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,14 @@ import Overview from './pages/Farmer/Overview';
 import Orders from './pages/Farmer/Orders';
 import Products from './pages/Farmer/Products';
 import FarmerLayout from './pages/Farmer/FarmerLayout';
+import Feedback from './pages/Admin/Feedback';
+import FlaggedItems from './pages/Admin/FlaggedItems';
+import VerificationQueue from './pages/Admin/VerificationQueue';
+import AdminLayout from './pages/Admin/AdminLayout';
+import CustomerLayout from './pages/Customer/CustomerLayout';
+import CartPage from './pages/Customer/CartPage';
+import OrderTracking from './pages/Customer/OrderTracking';
+import Homepage from './pages/Customer/Homepage';
 
 function App() {
   return (
@@ -21,6 +29,30 @@ function App() {
                 <Route path="products" element={<Products />} />
               </Routes>
             </FarmerLayout>
+          }
+        />
+        <Route
+          path="/admin/*"
+          element={
+            <AdminLayout>
+              <Routes>
+                <Route path="feedback" element={<Feedback />} />
+                <Route path="flagged-items" element={<FlaggedItems />} />
+                <Route path="verification-queue" element={<VerificationQueue />} />
+              </Routes>
+            </AdminLayout>
+          }
+        />
+        <Route
+          path="/customer/*"
+          element={
+            <CustomerLayout>
+              <Routes>
+                <Route path="homepage" element={<Homepage />} />
+                <Route path="cartitems" element={<CartPage />} />
+                <Route path="orders" element={<OrderTracking />} />
+              </Routes>
+            </CustomerLayout>
           }
         />
 
