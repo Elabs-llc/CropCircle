f6d201e0f56330f9e77ad0943c53f818bb8f4f3f
Esegbe-Able <esegbeable6@gmail.com>
2024-11-13 20:11:34 +0000

    update

--- client/src/auth/Admin/AdminLogin.jsx
+++ client/src/auth/Admin/AdminLogin.jsx
@@ -5,26 +5,26 @@ const AdminLogin = () => {
   const formData = {
     email: "",
     password: "",
-  }
-  
+  };
+
   const [error, setError] = React.useState("");
   const [form, setForm] = React.useState(formData);
-  const [emailError, setEmailError] = React.useState(""); 
+  const [emailError, setEmailError] = React.useState("");
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  function HandleChange(e){
-    setForm({...form,[e.target.name]:e.target.value})
-      if (!emailRegex.test(e.target.value)) {
-        setEmailError("Please Enter a valid email address");
-      }
-    
+  function HandleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+    if (!emailRegex.test(e.target.value)) {
+      setEmailError("Please Enter a valid email address");
+    }
+
     console.log(form);
   }
 
-  function HandleSubmit(e){
+  function HandleSubmit(e) {
     e.preventDefault();
-    if(form.email === "" || form.password === ""){
-      alert("Please fill the form")
+    if (form.email === "" || form.password === "") {
+      alert("Please fill the form");
       return;
     }
     // try{
@@ -42,8 +42,8 @@ const AdminLogin = () => {
     // catch(error){
     //   setError(error);
     // }
-  
-    console.log("Results",form);
+    setForm(formData);
+    console.log("Results", form);
   }
   return (
     <div
@@ -51,9 +51,13 @@ const AdminLogin = () => {
       style={{ backgroundImage: `url(${AdminBg})` }}
     >
       <div className="p-20 w-full max-w-lg bg-gray-300 rounded-lg shadow-lg">
-      <h1 className="font-bold text-3xl text-center text-green-600 mb-4">CROPCIRCLE</h1>
-      <h2 className="text-xl font-semibold text-center">Welcome Back, Admin</h2>
-      <p className="text-center mb-6">Sign In to continue</p>
+        <h1 className="font-bold text-3xl text-center text-green-600 mb-4">
+          CROPCIRCLE
+        </h1>
+        <h2 className="text-xl font-semibold text-center">
+          Welcome Back, Admin
+        </h2>
+        <p className="text-center mb-6">Sign In to continue</p>
 
         <form onSubmit={HandleSubmit} action="" className="space-y-4">
           <input
@@ -61,11 +65,15 @@ const AdminLogin = () => {
             placeholder="Email"
             name="email"
             onChange={HandleChange}
+            value={form.email}
             className="w-full p-3 rounded-md border border-gray-400 focus:outline-none focus:border-green-500"
           />
           <input
             type="password"
             placeholder="Password"
+            name="password"
+            onChange={HandleChange}
+            value={form.password}
             className="w-full p-3 rounded-md border border-gray-400 focus:outline-none focus:border-green-500"
           />
           <button
