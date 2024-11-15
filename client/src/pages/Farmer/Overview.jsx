98250ddbaaaf4e6d777327782722715687adf285
Your Name <you@example.com>
2024-11-15 09:12:44 +0000

    Added Overviewpage

--- client/src/pages/Farmer/Overview.jsx
+++ client/src/pages/Farmer/Overview.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import SalesAnalytics from '../../components/Farmer/SalesAnalytics';
 
 const Overview = () => {
-  return (
-    <div className='bg-red h-100'>
-      Overview
-      </div>
-  )
-}
+  const [products] = useState([
+    { name: 'Tomato', paymentMode: "Visa Card **** 4831", quantity: 100, price: 1.5, description: 'Completed' },
+    { name: 'Orange', paymentMode: "Visa Card **** 4831", quantity: 150, price: 1.2, description: 'Completed' },
+    { name: 'Banana', paymentMode: "Visa Card **** 4831", quantity: 120, price: 0.8, description: 'Pending' },
+    { name: 'Mango', paymentMode: "Visa Card **** 4831", quantity: 80, price: 2.0, description: 'Cancelled' },
+    { name: 'Grapes', paymentMode: "Visa Card **** 4831", quantity: 90, price: 2.5, description: 'Pending' }
+  ]);
 
-export default Overview
+  return (
+    <div>
+      <SalesAnalytics products={products} />
+    </div>
+  );
+};
+
+export default Overview;
