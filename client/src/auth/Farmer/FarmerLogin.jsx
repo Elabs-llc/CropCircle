94d86c38a1431af089040c9517b4f186b6bcb427
Arhin David Kwabena <davidarhin2005@gmail.com>
2024-11-15 00:02:11 +0000

    color changed in farmers orders page

--- client/src/auth/Farmer/FarmerLogin.jsx
+++ client/src/auth/Farmer/FarmerLogin.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import FarmerLoginBg from "../../assets/Background-images/young-asian-farmer-with-smartphone 1.png";
+import {useNavigate} from 'react-router-dom'
+
 
 const FarmerLogin = () => {
   const formData = {
@@ -9,6 +11,7 @@ const FarmerLogin = () => {
   const [form, setForm] = React.useState(formData);
   const [error, setError] = React.useState("");
   const [emailError, setEmailError] = React.useState("");
+  const navigate = useNavigate()
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   function Handlechange(e) {
@@ -46,18 +49,19 @@ const FarmerLogin = () => {
     // }
 
     console.log(form);
+    navigate('/farmer/overview')
   }
   return (
     <div
-      className="flex justify-center items-center lg:justify-start h-screen bg-cover lg:bg-auto bg-no-repeat bg-right"
+      className="flex items-center justify-center h-screen bg-right bg-no-repeat bg-cover lg:justify-start lg:bg-auto"
       style={{ backgroundImage: `url(${FarmerLoginBg})` }}
     >
       <div
-        className="flex flex-col items-center justify-center w-10/12 md:w-3/4 lg:w-1/2 lg:h-lvh lg:p-24 bg-white opacity-90 p-8 pb-12 md:p-16 border border-green-400 rounded-3xl lg:rounded-tl-none lg:rounded-bl-none "
+        className="flex flex-col items-center justify-center w-10/12 p-8 pb-12 bg-white border border-green-400 md:w-3/4 lg:w-1/2 lg:h-lvh lg:p-24 opacity-90 md:p-16 rounded-3xl lg:rounded-tl-none lg:rounded-bl-none "
       >
-        <h1 className="text-green-600 text-3xl font-bold">FARMER SIGN IN</h1>
-        <h2 className="text-xl font-semibold mt-4">Welcome Back</h2>
-        <p className="text-gray-500 mt-2">Sign in to continue</p>
+        <h1 className="text-3xl font-bold text-green-600">FARMER SIGN IN</h1>
+        <h2 className="mt-4 text-xl font-semibold">Welcome Back</h2>
+        <p className="mt-2 text-gray-500">Sign in to continue</p>
         <form onSubmit={HandleSubmit} action="" className="mt-8 space-y-4">
           <input
             type="email"
@@ -65,7 +69,7 @@ const FarmerLogin = () => {
             name="email"
             value={form.email}
             onChange={Handlechange}
-            className="w-full p-3 border border-gray-400 focus:outline-none focus:border-green-500 rounded"
+            className="w-full p-3 border border-gray-400 rounded focus:outline-none focus:border-green-500"
           />
           <input
             type="password"
@@ -73,11 +77,11 @@ const FarmerLogin = () => {
             name="password"
             value={form.password}
             onChange={Handlechange}
-            className="w-full p-3 border border-gray-400 focus:outline-none focus:border-green-500 rounded"
+            className="w-full p-3 border border-gray-400 rounded focus:outline-none focus:border-green-500"
           />
           <button
             type="submit"
-            className="w-full bg-green-600 text-white p-3 rounded-xl hover:bg-green-700"
+            className="w-full p-3 text-white bg-green-600 rounded-xl hover:bg-green-700"
           >
             Sign In
           </button>
